60207d33e248d50469e7203ee777e14064133547
JimmyL04 <89871662+JimmyL04@users.noreply.github.com>
2023-03-09 13:04:59 -0500

    Update lab_06_decode.py

--- lab_06_decode.py
+++ lab_06_decode.py
@@ -7,4 +7,24 @@ def decoder(password):
     return decode_password
 
 
+def menu():
+    print("Menu\n"
+          "-------------\n" 
+          "1. Encode\n" 
+          "2. Decode\n" 
+          "3. Quit\n")
 
+
+
+while True:
+    menu()
+    options= int(input("Please enter an option: "))
+    if options == 1:
+       pass
+    elif options == 2:
+        decoded = decoder()
+        print(f"The encoded password is {} , and the original password is {decoded}.")
+        print()
+
+    elif options == 3:
+        break
