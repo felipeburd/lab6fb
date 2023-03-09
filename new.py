5072a7ba939fbabf9d744d404a3c1f7ea7e7aae2
JimmyL04 <89871662+JimmyL04@users.noreply.github.com>
2023-03-09 12:53:06 -0500

    Update new.py

--- new.py
+++ new.py
@@ -21,12 +21,19 @@ while True:
     print("2. Decode")
     print("3. Quit")
     print(" ")
-    option = input("Please enter an option: ")
+    option = int(input("Please enter an option: "))
     if option == 1:
         password = input("Please enter your password to encode: ")
-        encode(password)
+        encoded=encode(password)
         print("Your password has been encoded and stored!")
-    elif option == 2: 
+        print()
+    elif option == 2:
+        decoded= decoder(encoded)
+        print(f"The encoded password is {encoded}, and the original password is {decoded}.")
+        print()
+
+    elif option ==3:
+        break
         
 
 
