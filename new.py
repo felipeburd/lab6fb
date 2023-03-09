43804c38454930aa7e784844c18122538c149322
JimmyL04 <89871662+JimmyL04@users.noreply.github.com>
2023-03-09 12:44:41 -0500

    Update new.py

--- new.py
+++ new.py
@@ -2,10 +2,17 @@ def encode(password):
     encoded_password = ""
     for num in password:
         encoded_password = encoded_password + str((int(num) + 3) % 10)
-    return encoded_password
+    return encoded_password 
 
 
-print(encode("12345555"))
+def decoder(password):
+    decode_password=""
+    for nums in password:
+        decode_password= decode_password +str((int(nums)-3) % 10)
+
+    return decode_password
+
+
 
 while True:
     print("Menu")
@@ -14,13 +21,14 @@ while True:
     print("2. Decode")
     print("3. Quit")
     print(" ")
-    option = input(print("Please enter an option: "))
+    option = input("Please enter an option: ")
     if option == 1:
         password = input("Please enter your password to encode: ")
         encode(password)
         print("Your password has been encoded and stored!")
-    elif option == 2:
-        pass
+    elif option == 2: 
+        
+
 
 
 
